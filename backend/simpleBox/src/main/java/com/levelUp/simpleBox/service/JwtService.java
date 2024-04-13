7c31ed45c3cef099b63d3705c98bc91eb616a3ff
Usmon <usmonpahlavonov@gmail.com>
2024-04-13 23:06:47 +0900

    jwt security config

---
+++ backend/simpleBox/src/main/java/com/levelUp/simpleBox/service/JwtService.java
@@ -0,0 +1,68 @@
+package com.levelUp.simpleBox.service;
+
+import java.util.Date;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import com.levelUp.simpleBox.model.User;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+    private final String SECRET_KEY = "3988fe1d4d32d38fff238e00b2b1b388a480460b814a3c4a4d133a4686a25421";
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean isValid(String token, UserDetails user){
+        String username = extractUsername(token);
+        return (username.equals(user.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return expirationDate(token).before(new Date());
+    }
+
+    private Date expirationDate(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> resolver){
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+            .parser()
+            .verifyWith(getSigninKey())
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+    }
+
+    public String generateToken(User user){
+        String token = Jwts
+            .builder()
+            .subject(user.getUsername())
+            .issuedAt(new Date(System.currentTimeMillis()))
+            .expiration(new Date(System.currentTimeMillis() + 24*60*60*1000))
+            .signWith(getSigninKey())
+            .compact();
+        return token;
+    }
+
+    private SecretKey getSigninKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
